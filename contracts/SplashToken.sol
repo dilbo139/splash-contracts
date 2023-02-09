7ae811691a72691bd3d42e3868beb0cad51350a5
Dilano Bo <dilano.bo@protonmail.com>
2023-02-09 16:28:19 +0530

    add 1% tax to splash token

--- contracts/SplashToken.sol
+++ contracts/SplashToken.sol
@@ -1,20 +1,41 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.17;
 
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import '@openzeppelin/contracts/token/ERC20/ERC20.sol';
 
 // import "@openzeppelin/token/ERC20/ERC20.sol";
 
 contract SplashToken is ERC20 {
-    constructor(
-        string memory name,
-        string memory symbol,
-        uint256 totalSupply
-    ) ERC20(name, symbol) {
-        // Mint `totalSupply` tokens to msg.sender
-        // Similar to how
-        // 1 dollar = 100 cents
-        // 1 token = 1 * (10 ** decimals)
-        _mint(msg.sender, totalSupply * 10**uint256(decimals()));
-    }
+  address public treasury;
+
+  constructor(
+    string memory name,
+    string memory symbol,
+    uint256 totalSupply,
+    address _treasury
+  ) ERC20(name, symbol) {
+    // Mint `totalSupply` tokens to msg.sender
+    // Similar to how
+    // 1 dollar = 100 cents
+    // 1 token = 1 * (10 ** decimals)
+    treasury = _treasury;
+    _mint(msg.sender, totalSupply * 10**uint256(decimals()));
+  }
+
+  /**
+@dev Apply a 1% tax on all transfers
+@param from The address of the sender
+@param to The address of the recipient
+@param amount The amount of the sender
+ */
+  function _transfer(
+    address from,
+    address to,
+    uint256 amount
+  ) internal override {
+    uint256 tax = (amount * 100) / 10_000;
+    amount -= tax;
+    super._transfer(from, treasury, tax);
+    super._transfer(from, to, amount);
+  }
 }
--- test/SplashTokenTest.sol
+++ test/SplashTokenTest.sol
@@ -1,62 +1,60 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.17;
-import "forge-std/Test.sol";
-import "../contracts/SplashToken.sol";
+import 'forge-std/Test.sol';
+import '../contracts/SplashToken.sol';
 
 contract SplashTokenTest is Test {
-    address public alice = address(0x0);
-    address public bob = address(0x1);
-    SplashToken public token;
+  address public alice = address(0x1);
+  address public bob = address(0x2);
+  address public treasury = address(0x3);
+  SplashToken public token;
 
-    /// @notice Events
-    event Transfer(address indexed from, address indexed to, uint256 value);
-    event Approval(
-        address indexed owner,
-        address indexed spender,
-        uint256 value
-    );
+  /// @notice Events
+  event Transfer(address indexed from, address indexed to, uint256 value);
+  event Approval(address indexed owner, address indexed spender, uint256 value);
 
-    function setUp() public {
-        token = new SplashToken("Splash", "SPLSH", 100000000);
-    }
+  function setUp() public {
+    token = new SplashToken('Splash', 'SPLSH', 100000000, treasury);
+  }
 
-    function testName() public {
-        assertEq(token.name(), "Splash");
-    }
+  function testName() public {
+    assertEq(token.name(), 'Splash');
+  }
 
-    function testSymbol() public {
-        assertEq(token.symbol(), "SPLSH");
-    }
+  function testSymbol() public {
+    assertEq(token.symbol(), 'SPLSH');
+  }
 
-    function testTotalSupply() public {
-        assertEq(token.totalSupply(), 100000000 * 10**18);
-    }
+  function testTotalSupply() public {
+    assertEq(token.totalSupply(), 100000000 * 10**18);
+  }
 
-    function testTransferFromToken() public {
-        address sender = address(this);
-        uint256 amount = 1;
+  function testTransferFromToken() public {
+    address sender = address(this);
+    uint256 amount = 50;
 
-        vm.expectEmit(true, true, false, true);
-        emit Approval(sender, sender, amount);
-        token.approve(sender, amount);
+    vm.expectEmit(true, true, false, true);
+    emit Approval(sender, sender, amount);
+    token.approve(sender, amount);
 
-        vm.expectEmit(true, true, false, true);
-        emit Transfer(sender, bob, amount);
-        token.transferFrom(sender, bob, amount);
+    vm.expectEmit(true, true, false, true);
+    emit Transfer(sender, bob, amount);
+    token.transferFrom(sender, bob, amount);
 
-        uint256 bobBalance = token.balanceOf(bob);
-        assertEq(bobBalance, amount);
-    }
+    uint256 bobBalance = token.balanceOf(bob);
+    assertEq(bobBalance, amount);
+  }
 
-    function testTransferToken() public {
-        uint amount = 1;
+  function testTransferToken() public {
+    uint256 amount = 200;
+    uint256 tax = (amount * 100) / 10_000;
 
-        assertEq(token.balanceOf(bob), 0);
+    assertEq(token.balanceOf(bob), 0);
 
-        vm.expectEmit(true, true, false, true);
-        emit Transfer(address(this), bob, amount);
-        token.transfer(bob, amount);
+    vm.expectEmit(true, true, false, true);
+    emit Transfer(address(this), bob, amount - tax);
+    token.transfer(bob, amount);
 
-        assertEq(token.balanceOf(bob), amount);
-    }
+    assertEq(token.balanceOf(bob), amount - tax);
+  }
 }
